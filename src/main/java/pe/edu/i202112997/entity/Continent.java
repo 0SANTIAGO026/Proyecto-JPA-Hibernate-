506c86f8a778cc1d0c4f7e086c1c3e0df77e7961
Eddy Santiago Gutierrez Riveros <lleddysantiagoll@outlook.com>
2024-11-20 10:54:25 -0500

    Primer commit con todos los archivos

---
+++ src/main/java/pe/edu/i202112997/entity/Continent.java
@@ -0,0 +1,11 @@
+package pe.edu.i202112997.entity;
+
+public enum Continent {
+    Asia,
+    Europe,
+    North_America,
+    Africa,
+    Oceania,
+    Antarctica,
+    South_America;
+}
---
+++ src/main/java/pe/edu/i202112997/entity/IsOfficial.java
@@ -0,0 +1,6 @@
+package pe.edu.i202112997.entity;
+
+public enum IsOfficial {
+    T,
+    F;
+}
